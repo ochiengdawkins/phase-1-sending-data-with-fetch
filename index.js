0298671a2a8cd2cc6fde5551260bfbd46898d0fb
DawkinsJonathanOchieng <dawkinsjonathan871@gmail.com>
2022-08-25 19:00:31 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(userName, userEmail){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    })
+    .then(function(respone){return respone.json()})
+    .then((object)=>domFunc(object))
+    .catch((error)=>{
+        const obj =  document.createElement("div");
+        obj.textContent = error.message;
+        document.body.appendChild(obj)
+    })
+}
+function domFunc(res){
+    console.log(res);
+    const obj =  document.createElement("div");
+    obj.textContent = res.id;
+    document.body.appendChild(obj)
+}
